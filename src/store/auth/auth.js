d7f3517ad2e6769537682cc4af1181f8423e1096
HaiOliver <hhuynh28@my.bcit.ca>
2021-04-15 01:17:28 -0700

    read,learn more some examples about authentication using state management in Vuex, creating auth.js in store folder

---
+++ src/store/auth/auth.js
@@ -0,0 +1,79 @@
+import {Auth} from "aws-amplify";
+export const auth = {
+      namespace : true,
+      state : {user : null},
+      mutation:{
+            setUser(state, payload){
+                  state.user = payload
+                  }
+            },
+      actions:{
+            async loadingContainer({commit,dispatch},{username,password}){
+
+                  try {
+                        // ! user sign in AWS
+                        await Auth.singIn({
+                              username,
+                              password
+                        });
+
+                        const currentUser = await Auth.currentUserInfo()
+                  // ! commit state
+                  commit("setUser",currentUser)
+                  return Promise.resolve("Success")
+
+                  } catch(err){
+                        console.log("🚀 ~ file: auth.js ~ line 27 ~ loadingContainer ~ error", error)
+                        Promise.reject(err)
+
+                  }
+            },
+
+            // ! Log out
+            async logout({commit}){
+                  // ? remove User
+                  commit("setUser", null)
+                  return await Auth.signOut()
+
+            },
+
+            // Confirm Sign Up
+            async confirmSignUp(_,{username, code}){
+                  try {
+                        await Auth.confirmSignUp(username,code)
+                        return Promise.resolve()
+                  }catch(err){
+                        console.log("🚀 ~ file: auth.js ~ line 46 ~ confirmSignUp ~ err", err)
+                        Promise.reject(err)
+                  }
+            },
+
+            // Sign Up
+            async signUp(_,{username, password,email}){
+                  try {
+                        await Auth.signUp({
+                              username,
+                              password,
+                              attributes:{
+                                    email
+                              }
+                        })
+                        return Promise.resolve()
+                  }catch(err){
+                        console.log("🚀 ~ file: auth.js ~ line 58 ~ signUp ~ err", err)
+                        Promise.reject(err)
+                  }
+            },
+            async authAction({commit}){
+                  const currentUser = await Auth.currentUserInfo()
+                  commit("setUser", currentUser)
+            }
+
+
+
+      },
+      getters:{
+            user : state => state.user
+      }
+
+}
