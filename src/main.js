2c261da4f1789d94eca03468aaa2cfee748c167a
HaiOliver <hhuynh28@my.bcit.ca>
2021-04-15 00:40:39 -0700

    read through set up AWS and finish seting up AWS Amplify.Next step gonna touch authentication

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/tailwind.css";
 import router from "./router";
+import "./index.css";
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
+
+Amplify.configure(aws_exports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
 
 createApp(App).use(router).mount("#app");
