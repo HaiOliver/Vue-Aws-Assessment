93b84e9ae327fe8691af8262d4c8d2b599626210
HaiOliver <hhuynh28@my.bcit.ca>
2021-04-15 12:15:31 -0700

    add sign up page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
+
 import App from "./App.vue";
+// import store from './store'
 import "./assets/tailwind.css";
 import router from "./router";
 import "./index.css";
@@ -16,3 +18,4 @@ applyPolyfills().then(() => {
 });
 
 createApp(App).use(router).mount("#app");
+
